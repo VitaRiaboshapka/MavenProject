a29cba339617c87c193762ddfee0b4455a624af0
VitaRiaboshapka <v.riaboshapka@globallogic.com>
2021-09-27 19:38:51 +0300

    Task 1 is done.

---
+++ src/test/java/newClass/Test.java
@@ -0,0 +1,17 @@
+package newClass;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https:google.com");
+        driver.close();
+    }
+
+}
