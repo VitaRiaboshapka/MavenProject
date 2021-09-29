ac22148c4029122af2ad4adef381ebb41509364e
VitaRiaboshapka <v.riaboshapka@globallogic.com>
2021-09-29 11:03:45 +0300

    Task 1 is done.

--- src/test/java/newClass/Test.java
+++ src/test/java/newClass/Test.java
@@ -6,12 +6,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test {
     public static void main(String[] args) {
+        String baseUrl="https:google.com";
+
         WebDriverManager.chromedriver().setup();
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
 
-        driver.get("https:google.com");
+        driver.get(baseUrl);
         driver.close();
     }
+
 
 }
